0c6820970abaf521e2b7afe319e8db3319360110
henriquegf100 <henriquegf100@hotmail.com>
2024-06-27 18:34:32 -0300

    Burger project

---
+++ products.js
@@ -0,0 +1,9 @@
+const menuOptions = [
+{ name: 'X-Salada', price: 30, vegan: false, src: './asset/xsalada.png'},
+{ name: 'X-Bacon', price: 34, vegan: false, src: './asset/xbacon.png'},
+{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './asset/bacon-egg.png'},
+{ name: 'Monstruoso', price: 50, vegan: false, src: './asset/monstruoso.png'},
+{ name: 'Big Vegano', price: 55, vegan: true, src: './asset/xvegan.png'},
+{ name: 'X-Vegan', price: 45, vegan: true, src: './asset/monstruoso-vegan.png'},
+]
+
